c00aee29ea7c22d9c7e360013143179a13120e29
Eric Justesen <justesen.eric33@gmail.com>
2024-11-16 18:29:07 -0700

    temples page

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.style.display = (navigation.style.display === 'block') ? 'none' : 'block';
+  hamButton.classList.toggle('open');
+});
